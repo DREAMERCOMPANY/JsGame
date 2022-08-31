22325df75494f2527e4bfc3d88a9d7e14c444001
DREAMERCOMPANY <jhon98-ra@hotmail.com>
2022-08-31 14:24:26 -0500

    hiding html elements from Js for better UX

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -6,8 +6,17 @@ let vidasJugador = 3;
 
 
 function iniciarJuego(){
+
+    let sectionSeleccionarAtaque= document.getElementById('seleccionar-ataque');
+    sectionSeleccionarAtaque.style.display ='none';
+
+    let sectionReiniciar= document.getElementById('reiniciar');
+    sectionReiniciar.style.display='none'
+
     let botonMascotaJugador= document.getElementById('boton-mascota');
     botonMascotaJugador.addEventListener('click' , seleccionarMascotaJugador);
+
+    
     
     let botonFuego =document.getElementById('boton-fuego')
     botonFuego.addEventListener('click', ataqueFuego)
@@ -82,6 +91,8 @@ function combate(){
 
     revisarVidas()
 
+
+
 }
 
 function revisarVidas(){
@@ -115,14 +126,21 @@ function crearMensajeFinal(resultadoFinal){
     let botonAgua= document.getElementById('boton-agua')
     botonAgua.disabled= true
     let botonTierra= document.getElementById('boton-tierra')
-    botonTierra.disabled=true
+    botonTierra.disabled=true 
 
-    
-
-    
+    let sectionReiniciar= document.getElementById('reiniciar');
+    sectionReiniciar.style.display='block'
 }
 
 function seleccionarMascotaJugador(){
+
+    let sectionSeleccionarMascota= document.getElementById('seleccionar-mascota');
+    sectionSeleccionarMascota.style.display ='none';
+
+    let sectionSeleccionarAtaque= document.getElementById('seleccionar-ataque');
+    sectionSeleccionarAtaque.style.display ='block';
+
+
     let inputHipodogue= document.getElementById('hipodogue')
     let inputCapipepo= document.getElementById('capipepo');
     let inputRatihueya= document.getElementById('ratihueya');
