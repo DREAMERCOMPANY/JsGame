cf523747ab2ae8a30654393e491b69cc7fb74f92
DREAMERCOMPANY <jhon98-ra@hotmail.com>
2022-09-01 15:29:07 -0500

    Adapting Js to game style

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -106,20 +106,32 @@ function revisarVidas(){
 
 
 function crearMensaje(resultado){
-    let sectionMensajes= document.getElementById('mensajes')
-    let parrafo= document.createElement('p');
-    parrafo.innerHTML= 'Tu mascota ataco con ' + ataqueJugador + ' la mascota del enemigo ataco con ' + ataqueEnemigo + ' - ' + resultado;
-    sectionMensajes.appendChild(parrafo)
+    let sectionMensajes= document.getElementById('resultado')
+    let ataquesDelJugador= document.getElementById('ataques-del-jugador')
+    let ataquesDelEnemigo= document.getElementById('ataques-del-enemigo')
+
+   
+    let nuevoAtaqueDelJugador= document.createElement('p');
+    let nuevoAtaqueDelEnemigo= document.createElement('p');
+
+    sectionMensajes.innerHTML= resultado;
+    nuevoAtaqueDelJugador.innerHTML= ataqueJugador;
+    nuevoAtaqueDelEnemigo.innerHTML= ataqueEnemigo;
+
+   
+    ataquesDelJugador.appendChild(nuevoAtaqueDelJugador);
+    ataquesDelEnemigo.appendChild(nuevoAtaqueDelEnemigo);
     
 
 }
 
 function crearMensajeFinal(resultadoFinal){
     
-    let sectionMensajes= document.getElementById('mensajes');
-    let parrafo = document.createElement('p')
-    parrafo.innerHTML= resultadoFinal
-    sectionMensajes.appendChild(parrafo);
+    let sectionMensajes= document.getElementById('resultado');
+
+
+    sectionMensajes.innerHTML= resultadoFinal
+    
 
     let botonFuego =document.getElementById('boton-fuego')
     botonFuego.disabled= true
